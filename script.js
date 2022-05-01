a8f23566065c4eba9f467d6ae0d30f36e8643d96
Daniel de Oliveira <danieloliveira05101994@gmail.com>
2022-05-01 01:36:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const numbers = "0123456789";
+let output = document.getElementById("output");
+
+function randomValue(value) {
+  return Math.floor(Math.random() * value);
+}
+
+function genPassword() {
+  let length = document.getElementById("length").value;
+  document.getElementById("length-val").textContent = length;
+  let str = "";
+
+  for (let i = 0; i < length; i++) {
+    let random = randomValue(numbers.length);
+    str += numbers.charAt(random);
+  }
+  output.value = str;
+}
+
+function copyClipboard() {
+  output.select();
+  document.execCommand("copy");
+  alert("Password Copied!");
+}
+genPassword();
